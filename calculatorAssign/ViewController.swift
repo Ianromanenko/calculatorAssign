15d51683dc8dc80858645d8eaf407aff9c15a33d
yan <ianromanenko@gmail.com>
2017-09-22 14:23:14 -0400

    math actions added

--- calculatorAssign/ViewController.swift
+++ calculatorAssign/ViewController.swift
@@ -1,19 +1,41 @@
-//
-//  ViewController.swift
-//  calculatorAssign
-//
-//  Created by Yan on 2017-09-20.
-//  Copyright © 2017 Centennial College. All rights reserved.
-//
+/*
+// Author: Yan Romanenko
+// App: Calculator
+// Date: 22nd September 2017
+// Version: 0.2 math actions added
+ */
 
 import UIKit
 
 class ViewController: UIViewController {
 
-    @IBOutlet weak var resultLabel: UILabel!
-    var typing = false;
-        
     
+    // Outlets
+    @IBOutlet weak var resultLabel: UILabel!
+    
+    
+    
+    
+    // !!!!!!!!!!
+    var typing = false;
+    var firstOperand: Double = 0;
+    var secondOperand: Double = 0;
+    var operationSign = "";
+    var currentNumber: Double {
+        get{
+            return Double(resultLabel.text!)!;
+        }
+        set{
+            resultLabel.text = "\(newValue)";
+            typing = false;
+        }
+    };
+    
+    
+    
+    
+    
+    // Actions
     @IBAction func numberPressed(_ sender: UIButton) {
         let number = sender.currentTitle!
         
@@ -25,6 +47,13 @@ class ViewController: UIViewController {
         }
     }
     
+    @IBAction func twoOperandsMath(_ sender: UIButton) {
+        operationSign = sender.currentTitle!
+        firstOperand = currentNumber;
+        print(firstOperand);
+        typing = false;
+    }
+
     
     
     override func viewDidLoad() {
